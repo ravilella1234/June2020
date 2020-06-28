4be20edf480bda3ef0d5af1cf975a0f9169243cb
maha <maha@gmail.com>
2020-06-28 07:42:02 +0530

    added a,b,c variables in class4 hyderabad

--- Project4/src/pack4/Class4.java
+++ Project4/src/pack4/Class4.java
@@ -1,7 +1,9 @@
 package pack4;
 
-public class Class4 {
-
+public class Class4 
+{
+	int a=100,b=200,c;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iam Class4");
